a4aba10255e8503bad74f6f08be75d5ef63d60ff
longfengpili <398745129@qq.com>
2019-11-14 18:48:54 +0800

    [新增]pytest

---
+++ __init__.py
@@ -0,0 +1,10 @@
+'''
+@Author: longfengpili
+@Date: 2019-11-14 18:48:13
+@LastEditTime: 2019-11-14 18:48:40
+@github: https://github.com/longfengpili
+'''
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+
