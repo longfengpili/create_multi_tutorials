a417db8028544f10432ef00170ed046e9b378d41
longfengpili <398745129@qq.com>
2019-12-12 09:46:02 +0800

    [新增]excel_api

---
+++ excel_api/__init__.py
@@ -0,0 +1,15 @@
+'''
+@Author: longfengpili
+@Date: 2019-11-11 18:32:24
+@LastEditTime: 2019-12-11 20:02:06
+@github: https://github.com/longfengpili
+'''
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+
+from .read_excel import ReadDataFromExcel
+from .write_excel import WriteDataToExcel
+
+
+__all__ = ['ReadDataFromExcel', 'WriteDataToExcel']
